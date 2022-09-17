814bdd4934aceb4a6995939d74a86fb8a1b37559
Yuu412 <yuu.yoshi12@outlook.jp>
2022-09-17 15:31:45 +0900

    Initial Commit

---
+++ Tango/TangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangoApp.swift
+//  Tango
+//
+//  Created by Yoshida Yuya on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct TangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
